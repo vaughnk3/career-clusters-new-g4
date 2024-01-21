7d4abde2997817d690c7f9d128508b1cf743c7d0
Red0ctober2002 <friendr3@mailbox.winthrop.edu>
2024-01-21 17:05:00 -0500

    Test

--- career-clusters/src/components/StaffComponents/ManagementPages/ClusterManagementPage.js
+++ career-clusters/src/components/StaffComponents/ManagementPages/ClusterManagementPage.js
@@ -1,10 +1,10 @@
 import TopRectangle from "../../page_Components/TopRectangle";
 import BottomRectangle from "../../page_Components/BottomRectangle";
 import './ClusterManagementPage.css';
+import OverlayRectangle from "../OverlayRectangle";
 
 
 const ClusterManagementPage = () => {
-
     return (
         <div>
             <TopRectangle/>
