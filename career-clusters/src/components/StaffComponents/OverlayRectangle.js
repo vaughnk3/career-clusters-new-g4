cc05b81a2ad3e2816810a4c94ae19b14c70e6e85
Red0ctober2002 <friendr3@mailbox.winthrop.edu>
2024-01-21 17:00:51 -0500

    Overlay rectangle to prevent view of usericon

---
+++ career-clusters/src/components/StaffComponents/OverlayRectangle.js
@@ -0,0 +1,12 @@
+import './OverlayRectangle.css'
+
+const OverlayRectangle = () => {
+    return(
+        <div className = "overlay" >
+            
+        </div>
+    )
+}
+
+
+export default OverlayRectangle;
