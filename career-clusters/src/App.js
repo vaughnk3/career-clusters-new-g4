c6df0508d8768e132f6f80b11e20e65b59b01133
Red0ctober2002 <friendr3@mailbox.winthrop.edu>
2024-01-21 16:32:46 -0500

    Added new routes

--- career-clusters/src/App.js
+++ career-clusters/src/App.js
@@ -7,6 +7,14 @@ import LoginPage from './components/login_components/LoginPage.js';
 import ClusterPage from './components/cluster_Components/ClusterPage';
 import SubFieldsPage from './components/SubFieldInfo/SubFieldPage';
 import SubClusterPage from './components/subCluster_Components/SubClusterPage.js';
+import StaffClusters from './components/StaffComponents/StaffClusters';
+import StaffSubClusters from './components/StaffComponents/StaffSubClusters';
+import StaffSubFields from './components/StaffComponents/StaffSubFields';
+import JobListingPage from './components/JobListingComponents/JobListingPage';
+import StaffJobListingPage from './components/StaffComponents/StaffJobListingPage';
+import AdminLandingPage from './components/StaffComponents/AdminLandingPage';
+import ModifyPermsPage from './components/StaffComponents/AdminPrivComponents/ModifyPermsPage';
+import CreateStaffAccount from './components/StaffComponents/AdminPrivComponents/CreateStaffAccount';
 
 function App() {
   return (
@@ -14,9 +22,20 @@ function App() {
       <Routes>
         <Route path="/" element={<DemographicInfo />} />
         <Route path="/cluster" element={<ClusterPage />} />
-        <Route path="/login" element={<LoginPage />} />
         <Route path="/cluster/subcluster" element={<SubClusterPage/>} />
         <Route path="/cluster/subcluster/subclusterinfo" element={<SubFieldsPage/>} />
+        <Route path="/cluster/subcluster/subclusterinfo/joblistings" element={<JobListingPage/>} />
+
+        <Route path="/login" element={<LoginPage />} />
+
+        <Route path="/login/staffclusters" element={<StaffClusters/>} />
+        <Route path="/login/staffclusters/staffsubclusters" element={<StaffSubClusters/>} />
+        <Route path="/login/staffclusters/staffsubclusters/staffsubclusterinfo" element={<StaffSubFields/>} />
+        <Route path="/login/staffclusters/staffsubclusters/staffsubclusterinfo/staffjoblistings" element={<StaffJobListingPage/>} />
+
+        <Route path="/login/adminpage" element={<AdminLandingPage/>} />
+        <Route path="/login/adminpage/modifyperms" element={<ModifyPermsPage/>} />
+        <Route path="/login/adminpage/createstaffpage" element={<CreateStaffAccount/>} />
       </Routes>
     </Router>
   );
