52dbc6946bfa94bc3e9eaedfc1971912ef5d0690
Red0ctober2002 <friendr3@mailbox.winthrop.edu>
2024-01-24 19:13:25 -0500

    getting some more management stuff in place

--- career-clusters/src/components/StaffComponents/ManagementPages/ClusterManagementPage.js
+++ career-clusters/src/components/StaffComponents/ManagementPages/ClusterManagementPage.js
@@ -5,6 +5,7 @@ import OverlayRectangle from "../OverlayRectangle";
 import ManagementCluster from "./ManagementCluster";
 import React, { useState, useEffect } from "react";
 import { useNavigate } from 'react-router-dom';
+import './PopUps.css';
 
 
 const ClusterManagementPage = () => {
@@ -28,14 +29,15 @@ const ClusterManagementPage = () => {
     }, []);
 
 
-
     return (
         <div id="page">
+            <link rel="stylesheet" href="./PopUps.css"></link>
             <div id="_topRectangle">
+                <button id="add_cluster" >Add Cluster</button>
                 <h2>Cluster Management Page</h2>
                 <h4>Please select an option for cluster management.</h4>
             </div>
-
+            <br></br><br></br><br></br><br></br><br></br><br></br>
             <ul>
                     {clusters.map((clusters) => (
                         <li>
@@ -45,8 +47,8 @@ const ClusterManagementPage = () => {
                 </ul>
 
             <BottomRectangle/>
-        </div>
 
+        </div>
 
 
         
