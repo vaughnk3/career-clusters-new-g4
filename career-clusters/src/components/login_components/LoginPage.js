d92eae518aa3fc1fb524cd9322267bd28523cad5
andersong6 <andersong6@mailbox.winthrop.edu>
2024-01-30 22:28:52 -0500

    working on login page and implementing firebase

--- career-clusters/src/components/login_components/LoginPage.js
+++ career-clusters/src/components/login_components/LoginPage.js
@@ -2,11 +2,69 @@ import React from 'react';
 import TopLeftLogo from '../page_Components/TopLeftLogo';
 import BottomRectangle from '../page_Components/BottomRectangle';
 import './LoginPage.css'
+import {initializeApp} from 'firebase/app';
+
+//Replace this with firebase config info :) (Not sure what some of these fields would be)
+const firebaseConfig = {
+    apiKey: "", //Project's API key (Waiting on information)
+    authDomain:: "", //Authentication domain
+    projectId: "", //Firebase project ID
+    storageBucket: "", //Project's storage bucket
+    messagingSenderId: "", //Firebase messaging sender ID
+    appId: "", //Firebase app ID
+};
+
+//Initialize Firebase Config
+const app = initializeApp(firebaseConfig);
+
+
+
 
 
 
 const LoginPage = () => {
+    const[username, setUsername] = useState('');
+    const[password, setPassword] = useState('');
+
+    const handleSubmit = (event) => {
+        event.preventDefault();
+    }
+
     return (
+        <div id="page">
+          <div id="topbar">
+            <TopLeftLogo />
+          </div>
+          <div id="content">
+            <div id="form">
+              <form onSubmit={handleSubmit}>
+                <label htmlFor="usernameField"><strong>Username</strong></label><br />
+                <input
+                  type="text"
+                  className="field"
+                  placeholder='Input username here'
+                  value={username}
+                  onChange={(e) => setUsername(e.target.value)}
+                /><br />
+                <label htmlFor="passwordField"><strong>Password</strong></label><br />
+                <input
+                  type="password"
+                  className="field"
+                  placeholder='Input password here'
+                  value={password}
+                  onChange={(e) => setPassword(e.target.value)}
+                /><br />
+                <p>Forgot Password?</p>
+                <button type="submit" id="login-button">Login</button>
+              </form>
+            </div>
+          </div>
+    
+          <BottomRectangle />
+        </div>
+      );
+
+   /* return (
         <div id="page">
             <div id="topbar">
             <TopLeftLogo/>
@@ -26,7 +84,7 @@ const LoginPage = () => {
 
         <BottomRectangle/>
         </div>
-    )
+    )*/
 };
 
 
