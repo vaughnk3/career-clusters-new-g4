c64ce904fb8cf90073f0bfb9503db25d56c9431f
Kevin Vaughn <vaughnk3@winthrop.edu>
2024-02-20 21:47:49 -0500

    Added conditional checking to see if user is logged in or admin

--- career-clusters/src/components/StaffComponents/AdminProtectedRoute.js
+++ career-clusters/src/components/StaffComponents/AdminProtectedRoute.js
@@ -1,24 +1,39 @@
 import React, { useEffect, useState } from 'react';
-import { Navigate, useLocation } from 'react-router-dom';
+import { Navigate, useNavigate, useLocation } from 'react-router-dom';
 import { getAuth, onAuthStateChanged } from 'firebase/auth';
 import './AdminProtectedRoute.css'
 
 const AdminProtectedRoute = ({ children }) => {
+    const navigate = useNavigate();
     const [user, setUser] = useState(null);
     const [loading, setLoading] = useState(true);
     const location = useLocation();
-    const adminUID = 'NW0QYGlDcaRCgEk8T8r9n3MgvP22';
+    const adminUID = 'NW0QYGlDcaRCgEk8T8r9n3MgvP22'; //change when passing off to YCRC
 
     useEffect(() => {
         const auth = getAuth();
         onAuthStateChanged(auth, (currentUser) => {
-            if(currentUser && currentUser.uid === adminUID) {
-                console.log('Admin user is logged in: ', currentUser)
-                setUser(currentUser);
+            if(currentUser) {
+                if(currentUser.uid === adminUID) {
+                    setUser(currentUser);
+                    console.log('Admin user is logged in: ', currentUser);
+                } else {
+                    setLoading(false);
+                    console.log('User is logged in. Redirecting to staffclusters: ', currentUser);
+                    navigate('/login/staffclusters');
+                    return;
+                }
             } else {
-                console.log('Failed admin authentication. Redirecting.')
                 setUser(null);
             }
+            // if(currentUser && currentUser.uid === adminUID) {
+            //     console.log('Admin user is logged in: ', currentUser)
+            //     setUser(currentUser);
+            
+            // } else {
+            //     console.log('Failed admin authentication. Redirecting.')
+            //     setUser(null);
+            // }
             setLoading(false);
         });
     }, []);
@@ -31,7 +46,7 @@ const AdminProtectedRoute = ({ children }) => {
     if(!user){
         console.log("user not logged in. byeeee nerd back to the lobby")
         return <Navigate to="/login" state={{ from: location }} replace />;
-    }
+    } 
     return children;
 };
 
